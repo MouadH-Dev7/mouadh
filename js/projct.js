e1fdbe5e96a6bd0b6ccede099b9bc0dc92fc25c8
MouadH-Dev7 <mouadh.dev@gmail.com>
2024-07-04 07:15:25 -1100

    First Commit

---
+++ js/projct.js
@@ -0,0 +1,75 @@
+var projcts = [
+    {
+        img: "/images/Coding workshop-bro.png",
+        titele: "p1",
+        description: "This is a project 1",
+
+    },
+    {
+        img: "/images/Coding workshop-bro.png",
+        titele: "p2",
+        description: "This is a project 1",
+
+    },
+    {
+        img: "/images/Coding workshop-bro.png",
+        titele: "p1",
+        description: "This is a project 1",
+
+    },
+    {
+        img: "/images/Coding workshop-bro.png",
+        titele: "p2",
+        description: "This is a project 1",
+
+    },
+    {
+        img: "/images/Coding workshop-bro.png",
+        titele: "p1",
+        description: "This is a project 1",
+
+    },
+    {
+        img: "/images/Coding workshop-bro.png",
+        titele: "p2",
+        description: "This is a project 1",
+
+    },
+    {
+        img: "/images/Coding workshop-bro.png",
+        titele: "p1",
+        description: "This is a project 1",
+
+    },
+    {
+        img: "/images/Coding workshop-bro.png",
+        titele: "p2",
+        description: "This is a project 1",
+
+    },
+    {
+        img: "/images/Coding workshop-bro.png",
+        titele: "p1",
+        description: "This is a project 1",
+
+    },
+    {
+        img: "/images/Coding workshop-bro.png",
+        titele: "p2",
+        description: "This is a project 1",
+
+    },
+    {
+        img: "/images/Coding workshop-bro.png",
+        titele: "p1",
+        description: "This is a project 1",
+
+    },
+    {
+        img: "/images/Coding workshop-bro.png",
+        titele: "p2",
+        description: "This is a project 1",
+
+    },
+
+]
